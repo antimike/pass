a2bdbd978f2b94b05793e9ad36307087b80517ca
Michael Haynes <antihaynes@gmail.com>
2021-08-13 11:27:23 -0600

    Added secure-edit script and .vimrc

---
+++ .extensions/secure-edit.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Decrypts, edits, and re-encrypts a file encrypted with GPG
+# TODO: Replace `vipe` with direct `nvim` invocation?
+
+source $BASH_INCLUDE
+
+set -e
+set -o pipefail
+
+typeset file="$1"
+fifo="$(mktemp)" && rm -f $fifo && mkfifo $fifo ||
+    die 27 "Unable to create FIFO"
+
+typeset key_regex="ID \([A-Z0-9]\+\)"
+if [ -f "$file" ]; then
+    gpg -d "$file" 2>&1 |
+        tee >(grep -o "$key_regex" | cut -f2 -d' ' >${fifo} &) |
+        sed -n '3,$p' |
+        vipe |
+        gpg -e -a -r $(cat <${fifo}) >"$file"
+else
+    die 23 "File '$file' does not exist"
+fi
+
+exit $?
---
+++ .extensions/secure-edit.vim
@@ -0,0 +1,7 @@
+set nobackup
+set nowritebackup
+set noswapfile
+set viminfo=
+if has('persistent_undo')
+  set noundofile
+endif
